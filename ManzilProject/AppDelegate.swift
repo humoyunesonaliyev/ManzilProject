0376fe96e41c1680e9d779e8f3c438abcd6fb477
Humoyun Esonaliyev <humoyunesonaliyev01@gmail.com>
2023-02-23 20:22:42 +0500

    Added LoginViewController

--- ManzilProject/AppDelegate.swift
+++ ManzilProject/AppDelegate.swift
@@ -1,3 +1,9 @@
+//
+//  AppDelegate.swift
+//  ManzilProject
+//
+//  Created by Humoyun Esonaliyev on 23/02/23.
+//
 
 import UIKit
 
@@ -5,23 +11,21 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let loginVC = LoginVC(nibName: "LoginVC", bundle: nil)
+        
+        window?.rootViewController = loginVC
+        
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        
-    }
+   
 
 
 }
---
+++ ManzilProject/LoginVC.swift
@@ -0,0 +1,29 @@
+//
+//  LoginVC.swift
+//  ManzilProject
+//
+//  Created by Humoyun Esonaliyev on 23/02/23.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
